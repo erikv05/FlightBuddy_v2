13464cc5378c710bd272ef12bf5d293b48f917d4
erikv05 <erikv05@uw.edu>
2023-12-01 09:08:29 -0800

    use v3 model

--- flask-server/predict.py
+++ flask-server/predict.py
@@ -12,7 +12,7 @@ FLIGHTS_API_KEY = os.getenv("FLIGHTS_API_KEY")
 
 session = HTMLSession()
 airports = pd.read_csv("airports.csv", index_col='iata')
-model = pickle.load(open('flights_model_v2.pkl', 'rb'))
+model = pickle.load(open('flights_model_v3.pkl', 'rb'))
 
 '''
 Example values:
